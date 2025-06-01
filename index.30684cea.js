c6ea167546858ed8b103aa5147adebc664a2433d
Viktoriia <viktoriamyhailiak@gmail.com>
2025-06-01 18:51:59 +0300

    make build

---
+++ index.30684cea.js
@@ -0,0 +1,2 @@
+var e=Array.from(document.querySelectorAll("input")),t=!0,r=!1,l=void 0;try{for(var a,n=e[Symbol.iterator]();!(t=(a=n.next()).done);t=!0){var o=a.value,i=document.createElement("label");i.className="field-label",i.setAttribute("for",o.id),i.textContent=o.name,o.parentElement.append(i),o.setAttribute("placeholder",o.name[0].toUpperCase()+o.name.slice(1))}}catch(e){r=!0,l=e}finally{try{t||null==n.return||n.return()}finally{if(r)throw l}}
+//# sourceMappingURL=index.30684cea.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const inputs = Array.from(document.querySelectorAll("input"));
+for (const input of inputs){
+    const label = document.createElement("label");
+    if (input.hasAttribute("id") && input.id !== "") label.setAttribute("for", input.id);
+    if (input.hasAttribute("name") && input.name !== "") {
+        label.textContent = input.name;
+        input.setAttribute("placeholder", input.name[0].toUpperCase() + input.name.slice(1));
+    }
+    label.className = "field-label";
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
